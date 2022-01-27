dc4d6aa13b5aac59e7988f71ff7cacc9a3438484
saber <saber.tabatabaee@gmail.com>
2022-01-27 23:39:23 +0330

    fix: migration installation problem

--- database/migrations/2021_07_16_125430_add_mobile_to_users.php
+++ database/migrations/2021_07_16_125430_add_mobile_to_users.php
@@ -14,7 +14,7 @@ class AddMobileToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('mobile');
+            $table->string('mobile')->nullable();
         });
     }
 
